c7bf24bbcf5f54633852d1188e8a418dd9e8eb95
Coldee shiashi <64113136+What-Cunt@users.noreply.github.com>
2021-10-05 07:52:26 +0000

    0.01

--- a.py
+++ a.py
@@ -1,1 +1,8 @@
-print('hi')
+from pytube import YouTube 
+##How to convert webm files to Mp3, had the capabilities to convert to FLAC(hi rez audio)
+# https://cloudconvert.com/webm-to-mp3
+yt = YouTube('https://youtu.be/z15wKo0r-74')
+print(yt.streams.filter(file_extension=''))
+stream = yt.streams.get_by_itag(int(input()))
+input('enter to continue')
+stream.download()
