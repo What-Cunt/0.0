1bf943d4393188578dfaa945755660590c818512
Coldee shiashi <64113136+What-Cunt@users.noreply.github.com>
2021-10-05 20:43:14 +0000

    Run through the filters

--- a.py
+++ a.py
@@ -1,8 +1,21 @@
 from pytube import YouTube 
 ##How to convert webm files to Mp3, had the capabilities to convert to FLAC(hi rez audio)
 # https://cloudconvert.com/webm-to-mp3
+
+#Basic
 yt = YouTube('https://youtu.be/z15wKo0r-74')
-print(yt.streams.filter(file_extension=''))
+print('The title: ', yt.title, "\n")
+print('The Url', yt.thumbnail_url, '\n')
+
+#What is there to download
+print(yt.streams.filter(progressive=True), '\n')
+print(yt.streams.filter(adaptive=True), '\n')
+print(yt.streams.filter(only_audio=True), '\n')
+print(yt.streams.filter(file_extension='mp4'))
+# print(yt.streams.filter(file_extension=''))
+
+
+#Choosing what to download
 stream = yt.streams.get_by_itag(int(input()))
 input('enter to continue')
 stream.download()
