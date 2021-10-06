0be991409916bf0da13de18b03b9e3608be2b832
Coldee shiashi <64113136+What-Cunt@users.noreply.github.com>
2021-10-06 16:42:31 +0000

    Playlist works but the quality is dog

--- a.py
+++ a.py
@@ -1,7 +1,8 @@
 from pytube import YouTube 
+from pytube import Playlist
 ##How to convert webm files to Mp3, had the capabilities to convert to FLAC(hi rez audio)
 # https://cloudconvert.com/webm-to-mp3
-
+print('hi')
 #Basic
 yt = YouTube('https://youtu.be/z15wKo0r-74')
 print('The title: ', yt.title, "\n")
@@ -13,20 +14,12 @@ print(yt.streams.filter(progressive=True), '\n')
 print(yt.streams.filter(adaptive=True), '\n')
 print(yt.streams.filter(only_audio=True), '\n')
 print(yt.streams.filter(file_extension='mp4'))
-# print(yt.streams.filter(file_extension=''))
+# print(yt.streams.filter(file_extension=''))('wtf is happening dawg???')
 
-#Subtitles and Captions Tracks (undone)
-print(yt.captions, '/n')
-caption = yt.captions.get_by_language_code('en')
-print(caption.xml_captions)
-#Choo
 
-#Using playlists
-from pytube import Playlist
 p = Playlist('https://youtube.com/playlist?list=PLAUxsgLNM2Bt8apMzywvdzOJSzlfW4OQa')
 print(f'Downloading: {p.title}')
-for video in p.videos:
-  video.streams.first().download()
-#choosing which file/tag to downloadm = yt.streams.get_by_itag(int(input()))
+for video in p.videos: video.streams.first().download()
+#choosing wh file/tag to downloadm = yt.streams.get_by_itag(int(input()))
 input('enter to continue')
 stream.download()
