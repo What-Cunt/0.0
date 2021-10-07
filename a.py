a2a1c67c5b42c0793c7f95b09acc5b36f609b5b7
Coldee shiashi <64113136+What-Cunt@users.noreply.github.com>
2021-10-07 20:15:46 +0000

    Igor Album

--- a.py
+++ a.py
@@ -3,26 +3,26 @@ from pytube import Playlist
 ##How to convert webm files to Mp3, had the capabilities to convert to FLAC(hi rez audio)
 # https://cloudconvert.com/webm-to-mp3
 print('hi')
-p = Playlist('https://youtube.com/playlist?list=PLAUxsgLNM2Bt8apMzywvdzOJSzlfW4OQa')
+p = Playlist('https://youtube.com/playlist?list=OLAK5uy_mao9YHfZtBBTlxeT138lO0prcGHzNSWQM')
 yt = YouTube('https://youtu.be/z15wKo0r-74')
 
 #Basic
-def Basic():
-  print('The title: ', yt.title, "\n")
-  print('The Url', yt.thumbnail_url, '\n')
-  print('The title: ', p.title, "\n")
-  print('The Url', p.thumbnail_url, '\n')
+print('The title: ', yt.title, "\n")  
+print('The Url', yt.thumbnail_url, '\n')
+
 
 
 #What is there to download
 print(yt.streams.filter(progressive=True), '\n')
 print(yt.streams.filter(adaptive=True), '\n')
 print(yt.streams.filter(only_audio=True), '\n')
-# print(yt.streams.filter(file_extension='mp4'))
+print(yt.streams.filter(file_extension='mp3'))
 # print(yt.streams.filter(file_extension=''))('wtf is happening dawg???')
 
 #playlist 
 ### REFER TO READ.MD NOTES SECTION###
+print('The title: ', p.title, "\n")
+# print('The Url', p.thumbnail_url, '\n')
 print(f'Downloading: {p.title}')
 for video in p.videos: video.streams.get_by_itag(int(input())).download()
 
